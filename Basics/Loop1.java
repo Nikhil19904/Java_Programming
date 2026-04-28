a31d30c495c1901553d3601ea1e58a83410f8dc9
Nikhil <nc429529@gmail.com>
2026-04-28 11:29:00 +0530

    first message

---
+++ Basics/Loop1.java
@@ -0,0 +1,39 @@
+public class Loop1 {
+    public static void main(String[] args) {
+
+        // for loop
+        // for (int i = 0; i < 10; i++) {
+        // System.out.print(i + " ");
+        // }
+
+        // Nested for loop
+        // for (int i = 0; i < 10; i++) {
+        // for (int j = 0; j < 5; j++) {
+        // System.out.print(i);
+        // // System.out.print(j);
+
+        // }
+        // }
+        // while-loop
+        // int i = 0;
+        // while (i < 10) {
+        // System.out.print(i + " ");
+        // i++;
+        // }
+
+        // do-while loop
+        // int i = 0;
+        // do {
+        // System.out.print(i + " ");
+        // i++;
+        // } while (i <= 5);
+
+        // for-each loop
+        // int[] arr = { 10, 20, 30 };
+
+        // for (int num : arr) {
+        // System.out.print(num + " ");
+        // }
+    }
+
+}
