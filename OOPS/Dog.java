b449528b63604c93f41e73e099d8ace519f9d714
Nikhil <nc429529@gmail.com>
2026-04-30 11:26:49 +0530

    push now

---
+++ OOPS/Dog.java
@@ -0,0 +1,21 @@
+public class Dog {
+
+    // Instance variables
+    int size;
+    String breed;
+    String name;
+
+    // methods
+    void bark() {
+        System.out.println("Ruff");
+    }
+
+    public static void main(String[] args) {
+        Dog d = new Dog(); // make an new object
+        d.size = 40; // .opearator gives you access to an object' state and behaivour
+        d.bark();
+    }
+}
+
+// 9310615638
+// 9818598993
---
+++ OOPS/Notes.java
@@ -0,0 +1,31 @@
+// all java code is defined in a class
+// a class describes how to make an object of that class type....A class is like
+// blueprint
+// Instanc variables : things an object knows about itself is called
+// instance(object's state)
+// variables..they represent the state of an object
+// things an object does are called methods....they represent the behaivour of
+// an object(what an object can do ??)
+
+// Example : Alarm object have an instance variable to hold the
+// alarmTime.....and 2 methods for getting and setting the alarm time
+// .opearator gives you access to an object' state and behaivour
+
+// Syntax : ClassName objectName = new ClassName();
+
+// If a method takes a parameter,you must pass it something when you call it.
+// parameter is a variable that is used inside the body of the method....with
+// type and name
+// Arguments :actual values jo pass krte h------jab method call krte ho jab jo
+// values dete ho...add(5,10);
+
+// a method must declare a return type...a void type means that the method does
+// not return anything
+
+// Instance variables always get a default value....if you don't explicilty
+// assign a value to an instance variable or
+// youdon't call a setter method...the instance variable still has a
+// value(default value)
+
+// default value : integer - 0, floating poins - 0.0, booleans - false ,
+// references - null
